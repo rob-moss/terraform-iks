31a383852fa549729824d0e2af4b3fd175321b2c
scotttyso <tyscott@cisco.com>
2021-06-11 21:09:58 -0400

    attempt 11

--- iks-c1/iks_cluster/outputs.tf
+++ iks-c1/iks_cluster/outputs.tf
@@ -25,10 +25,9 @@ output "worker_profile" {
 output "kube_config" {
   description = "Kubernetes Configuration File."
   value       = trimspace(<<-EOT
-  %{if var.worker_desired_size == "0"~}${intersight_kubernetes_cluster_profile.cluster_without_worker[count.index].kube_config[0].kube_config}
-  %{else~}${intersight_kubernetes_cluster_profile.cluster_with_worker[count.index].kube_config[0].kube_config}
+  %{if var.worker_desired_size == "0"~}${intersight_kubernetes_cluster_profile.cluster_without_worker[0].kube_config}
+  %{else~}${intersight_kubernetes_cluster_profile.cluster_with_worker[0].kube_config}
   %{endif~}
   EOT
   )
-  count = 1
 }
