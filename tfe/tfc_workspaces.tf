8fabb222973f7d6a7f877dcd1b6ee9f84474acbe
scotttyso <tyscott@cisco.com>
2021-06-13 21:00:46 -0400

    removing optional attributes

--- tfe/tfc_workspaces.tf
+++ tfe/tfc_workspaces.tf
@@ -13,43 +13,27 @@ module "tfc_workspaces" {
   workspace_list = {
     "global_vars" = {
       auto_apply                = true
-      agent_pool                = ""
       description               = "Global Variables Workspace."
-      execution_mode            = "remote"
       global_remote_state       = true
       name                      = "${var.cluster_name}_global_vars"
-      queue_all_runs            = false
-      remote_state_consumer_ids = []
       terraform_version         = var.terraform_version
-      trigger_prefixes          = []
       vcs_repo                  = var.vcs_repo
       working_directory         = "global_vars"
     },
     "iks" = {
       auto_apply                = true
-      agent_pool                = ""
       description               = "Intersight Kubernetes Service Workspace."
-      execution_mode            = "remote"
-      global_remote_state       = false
       name                      = "${var.cluster_name}_iks"
-      queue_all_runs            = false
-      remote_state_consumer_ids = []
       terraform_version         = var.terraform_version
-      trigger_prefixes          = []
       vcs_repo                  = var.vcs_repo
       working_directory         = "iks"
     },
     "kube" = {
       auto_apply                = true
-      agent_pool                = ""
       description               = "Intersight Kubernetes Service - kube_config Workspace."
-      execution_mode            = "remote"
       global_remote_state       = true
       name                      = "${var.cluster_name}_kube"
-      queue_all_runs            = false
-      remote_state_consumer_ids = []
       terraform_version         = var.terraform_version
-      trigger_prefixes          = []
       vcs_repo                  = var.vcs_repo
       working_directory         = "kube"
     },
@@ -58,12 +42,8 @@ module "tfc_workspaces" {
       agent_pool                = module.tfc_agent_pool.tfc_agent_pool
       description               = "Application Workspace - Intersight Workload Optimizer."
       execution_mode            = "agent"
-      global_remote_state       = false
       name                      = "${var.cluster_name}_iwo"
-      queue_all_runs            = false
-      remote_state_consumer_ids = []
       terraform_version         = var.terraform_version
-      trigger_prefixes          = []
       vcs_repo                  = var.vcs_repo
       working_directory         = "iwo"
     },
@@ -72,26 +52,17 @@ module "tfc_workspaces" {
       agent_pool                = module.tfc_agent_pool.tfc_agent_pool
       description               = "Application Workspace - Hello Kubernetes."
       execution_mode            = "agent"
-      global_remote_state       = false
       name                      = "${var.cluster_name}_app_hello"
-      queue_all_runs            = false
-      remote_state_consumer_ids = []
       terraform_version         = var.terraform_version
-      trigger_prefixes          = []
       vcs_repo                  = var.vcs_repo
       working_directory         = "app_hello"
     },
     "remove" = {
       auto_apply                = true
-      agent_pool                = ""
       description               = "Workspace used to Destroy IKS Cluster."
       execution_mode            = "remote"
-      global_remote_state       = false
       name                      = "${var.cluster_name}_remove"
-      queue_all_runs            = false
-      remote_state_consumer_ids = []
       terraform_version         = var.terraform_version
-      trigger_prefixes          = []
       vcs_repo                  = var.vcs_repo
       working_directory         = "remove"
     },
